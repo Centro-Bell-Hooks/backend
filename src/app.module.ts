b2a89385c4987327f0802547e8ad752c7ab8e596
Fernando Cassio <fernandocassiodev@gmail.com>
2024-07-25 14:44:24 -0300

    feat: Adiciona entidade categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_projeto_integrador',
+      entities: [Categoria],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
