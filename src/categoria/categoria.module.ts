badea3c69143389d4eb47abd505eaf00a8f09759
Ezequiel <ezequiellalmeida2003@gmail.com>
2024-07-25 16:26:41 -0300

    Feat: Adicionado Classe Controller

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -2,7 +2,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Categoria } from "./entities/categoria.entity";
 import { Module } from "@nestjs/common";
 import { CategoriaService } from "./services/categoria.service";
-
+import { CategoriaController } from "./controllers/categoria.controller";
 @Module({
     imports: [TypeOrmModule.forFeature([Categoria])],
     providers: [CategoriaService],
--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,4 +1,5 @@
+import { Controller } from "@nestjs/common";
 import { Categoria } from "../entities/categoria.entity";
 
 @Controller()
-export class PostagemController{}
+export class CategoriaController{}
