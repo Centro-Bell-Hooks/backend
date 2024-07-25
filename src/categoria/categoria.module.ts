2cf2fe19161a21df5be6d1d3b42e86cd0f9f3646
Liara Cristina <lidskey@hotmail.com>
2024-07-25 16:05:28 -0300

    add: registro categoriaservice na categoria.module

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,13 +1,13 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Categoria } from "./entities/categoria.entity";
 import { Module } from "@nestjs/common";
-
+import { CategoriaService } from "./services/categoria.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Categoria])],
-    providers: [],
+    providers: [CategoriaService],
     controllers: [],
     exports: [TypeOrmModule]
 })
 
-export class PostagemModule{}
+export class CategoriaModule{}
